da3d3cb8fecfd52b17b8393905cc378193020829
riha7 <harik0725@gmail.com>
2021-07-20 20:41:03 +0530

    added h1 tags in app.js file

--- src/App.js
+++ src/App.js
@@ -4,6 +4,17 @@ function App() {
   return (
     <div className="App">
       <h1>this is new react page</h1>
+      <h1>this is new react page</h1>
+      <h1>this is new react page</h1>
+      <h1>this is new react page</h1>
+      <h1>this is new react page</h1>
+      <h1>this is new react page</h1>
+
+      <h1>this is new react page</h1>
+
+      <h1>this is new react page</h1>
+      <h1>this is new react page</h1>
+      <button>click</button>
     </div>
   );
 }
